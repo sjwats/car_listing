c4af3958ba4300790faed6087d6113dcebfc94f2
John <s.johnwatson@gmail.com>
2014-01-01 16:31:32 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CarListing::Application.config.secret_key_base = '196671bdf1c086f878ab72766cf62d538d5615a9d5e63476d9ea8d83816e0a075c742d4fbd22cb7d73790c3bcf4ccc01002935d38f6df39071ae7b7cad9db538'
